43d55d289747441e24b4e4202dd7977d3881f224
SAJI <wbajxw999@aliyun.com>
2016-06-04 20:57:05 +0800

    Add grid system & style examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,24 @@
 var React = require('react');
+var {Link} = require('react-router');
+
+var Examples = (props) => {
+  return (
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examples locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Shanghai'>Shanghai,China</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio,Brazil</Link>
+        </li>
+      </ol>
+    </div>
+  );
+};
+
+module.exports = Examples;
 
 // var Examples = React.createClass({
 //   render:function(){
@@ -7,14 +27,3 @@ var React = require('react');
 //     );
 //   }
 // });
-
-var Examples = (props) => {
-  return (
-    <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
-    </div>
-  );
-};
-
-module.exports = Examples;
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,6 +1,21 @@
 var React = require('react');
 var Nav = require('Nav');
 
+var Main = (props) => {
+  return(
+    <div>
+      <Nav />
+      <div className="row">
+        <div className="columns medium-6 large-4 small-centered">
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Main;
+
 // var Main = React.createClass({
 //   render:function(){
 //     return(
@@ -12,15 +27,3 @@ var Nav = require('Nav');
 //     );
 //   }
 // });
-
-var Main = (props) => {
-  return(
-    <div>
-      <Nav />
-      <h2>Main Component</h2>
-      {props.children}
-    </div>
-  );
-};
-
-module.exports = Main;
